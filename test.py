d7daa134e2471689193c1fb595c087807bebf69d
马龙·白兰度 <tangyubaozi@hotmail.com>
2016-04-26 14:32:27 +0800

    Initial commit

---
+++ test.py
@@ -0,0 +1,15 @@
+# ttyy's first python programme
+
+def test1():
+    '''Test will be done in this book.
+
+    So we can know many things in this line.'''
+    itself = "wo all know"
+    print("hello world!{0}我是这个呢",end='!'.format(itself))
+    print("hahaha")
+    return
+a = 48
+print ("si")
+test1()
+
+
